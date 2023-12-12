6aaa4611d70f0f0cfb8b42f166570523557f046a
phidalgo23 <phidalgo23@ilg.cat>
2023-12-12 17:14:00 +0100

    Practica Git

--- Program.java
+++ Program.java
@@ -4,7 +4,7 @@
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2020 Joan Sèculi <jseculi@escoladeltreball.org>
+ * Copyright 2020 Pol Hidalgo <phidalgo23@ilg.cat>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
@@ -45,7 +45,7 @@ public class Program {
 
 		Scanner in = new Scanner(System.in);
 
-		System.out.println("Enter the radius: ");
+		System.out.println("Enter the radius in meters: ");
 		double radi = in.nextDouble();
 
 		in.close();
